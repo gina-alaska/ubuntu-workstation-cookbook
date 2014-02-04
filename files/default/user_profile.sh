e39dc11eb0affa4a18ba82d7406a8c7ddb9c7c8e
Scott Macfarlane <scott@gina.alaska.edu>
2014-02-03 16:32:22 -0900

    Update user_profile

--- files/default/user_profile.sh
+++ files/default/user_profile.sh
@@ -1,2 +1,13 @@
-#This loops over everything in $HOME/.profile.d and brings it into your environment.
+# This file is managed by Chef
+# CHANGES WILL BE OVERWRITTEN
 
+#This loops over everything in $HOME/.profile.d and brings it into your environment.
+# Adapted from /etc/profile  Ubuntu 13.04
+if [ -d $HOME/.profile.d ]; then
+  for i in $HOME/.profile.d/*.sh; do
+    if [ -r $i ]; then
+      . $i
+    fi
+  done
+  unset i
+fi
--- test/integration/default/serverspec/user_profile_spec.rb
+++ test/integration/default/serverspec/user_profile_spec.rb
@@ -8,6 +8,7 @@ describe "user_profile" do
 	it 'creates profile.d script' do
 		expect(file("/etc/profile.d/user_profile.sh")).to be_file 
 		expect(file("/etc/profile.d/user_profile.sh")).to be_mode("755")
+		expect(file("/etc/profile.d/user_profile.sh")).to contain("# This file is managed by Chef")
 	end
 
 	#TODO:  Figure out how to use chef node attributes
