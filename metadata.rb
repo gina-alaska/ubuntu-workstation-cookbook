4330d960cd07ced656b23b86117481b9443e547b
Scott Macfarlane <scott@gina.alaska.edu>
2014-07-21 16:20:18 -0800

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'scott@gina.alaska.edu'
 license          'All rights reserved'
 description      'Installs/Configures ubuntu-workstation'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.4.0'
+version          '0.5.0'
 
 depends "apt"
 depends "user"
